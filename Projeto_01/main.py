0950ea9df7b2b34de413c109e2beb4c769ef543f
rcarvalho-pb <rcarvalho.pb@gmail.com>
2020-04-27 12:16:16 -0300

    completo

--- Projeto_01/main.py
+++ Projeto_01/main.py
@@ -1,37 +1,29 @@
 import pandas as pd
 import numpy as np
-import matplotlib as plt
 import sklearn
 from src import funcao as fc
-
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import confusion_matrix
 
 data = pd.read_csv('dataset/banco.csv')
-
-
-
 x,y = fc.fun(data)
 data = data.drop(['CHAVE'], axis = 1)
 
-
-from sklearn.model_selection import train_test_split
 training_set, validation_set = train_test_split(data, test_size = 0.2, random_state = 21)
-
-
 X_train = training_set.iloc[:,0:-1].values
 y_train = training_set.iloc[:,-1].values
 X_val = validation_set.iloc[:,0:-1].values
 y_val = validation_set.iloc[:,-1].values
 
-
-from sklearn.neural_network import MLPClassifier
 classifier = MLPClassifier(hidden_layer_sizes=(150,100,50), max_iter=300,activation = 'relu',solver='adam',random_state=1)
-
 classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_val)
-
-from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_pred, y_val)
 print('Accuracy of MLPClassifier : ', fc.accuracy(cm))
+
+fc.plot(data)
+
 
 
 
--- Projeto_01/src/funcao.py
+++ Projeto_01/src/funcao.py
@@ -9,3 +9,13 @@ def accuracy(confusion_matrix):
    diagonal_sum = confusion_matrix.trace()
    sum_of_all_elements = confusion_matrix.sum()
    return diagonal_sum / sum_of_all_elements
+
+def plot(k):
+    import matplotlib.pyplot as plt
+    corr=k.corr()
+    plt.imshow(corr, cmap='hot', interpolation='none')
+    plt.colorbar()
+    plt.xticks(range(len(corr)), corr.columns)
+    plt.yticks(range(len(corr)), corr.columns)
+    plt.savefig("output/correl.png")
+    plt.show()
